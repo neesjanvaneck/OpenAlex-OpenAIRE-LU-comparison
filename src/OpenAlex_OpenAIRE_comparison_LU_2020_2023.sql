8c9fd739d6b661a55cc899c0c7cabf55aae161a2
Nees Jan van Eck <36573334+neesjanvaneck@users.noreply.github.com>
2025-01-31 21:37:56 +0100

    Update based on new OpenAIRE data

--- src/OpenAlex_OpenAIRE_comparison_LU_2020_2023.sql
+++ src/OpenAlex_OpenAIRE_comparison_LU_2020_2023.sql
@@ -1,11 +1,11 @@
 -- openalex_openaire_doi_match_2020_2023
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023
 as
 (
   with openaire_pub as (
     select id as openaire_pub_id, json_value(pub_pid,'$.value') as doi
-    from openaire-graph.oag_2024_07.publications,
-    unnest(pids) as pub_pid
+    from openaire-graph.oag_2025_01.publications,
+    unnest(json_query_array(pids)) as pub_pid
     where json_value(pub_pid,'$.scheme') = 'doi'
   )
   select a.work_id as openalex_work_id, b.openaire_pub_id, lower(a.doi) as doi
@@ -15,19 +15,19 @@ as
 );
 
 -- openaire_pub_lu_2020_2023
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.openaire_pub_lu_2020_2023
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.openaire_pub_lu_2020_2023
 as
 (
   with openaire_org as (
     select id as openaire_org_id, json_value(org_pid,'$.value') as ror_id
-    from openaire-graph.oag_2024_07.organizations,
-    unnest(pids) as org_pid
+    from openaire-graph.oag_2025_01.organizations,
+    unnest(json_query_array(pids)) as org_pid
     where json_value(org_pid,'$.scheme') = 'ROR'
   )
   select distinct a.openaire_pub_id
   from
-    cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a,
-    openaire-graph.oag_2024_07.relations as b,
+    cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a,
+    openaire-graph.oag_2025_01.relations as b,
     openaire_org as c
   where a.openaire_pub_id = b.source
     and b.target = c.openaire_org_id
@@ -36,98 +36,98 @@ as
 );
 
 -- openalex_pub_lu_2020_2023
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.openalex_pub_lu_2020_2023
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.openalex_pub_lu_2020_2023
 as
 (
   select distinct a.openalex_work_id
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
   join cwts-leiden.openalex_2024aug.work_affiliation_institution as b on a.openalex_work_id = b.work_id
   join cwts-leiden.openalex_2024aug.institution as c on b.institution_id = c.institution_id
   where c.ror_id in ('027bh9e22', '05xvt9f17', '03es66g06')
 );
 
 -- openalex_doi_lu_2020_2023
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.openalex_doi_lu_2020_2023
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.openalex_doi_lu_2020_2023
 as
 (
   select distinct a.doi
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.openalex_pub_lu_2020_2023 as b on a.openalex_work_id = b.openalex_work_id
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.openalex_pub_lu_2020_2023 as b on a.openalex_work_id = b.openalex_work_id
 );
 
 -- openaire_doi_lu_2020_2023
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.openaire_doi_lu_2020_2023
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.openaire_doi_lu_2020_2023
 as
 (
   select distinct a.doi
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.openaire_pub_lu_2020_2023 as b on a.openaire_pub_id = b.openaire_pub_id
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.openaire_pub_lu_2020_2023 as b on a.openaire_pub_id = b.openaire_pub_id
 );
 
 -- doi_lu_2020_2023_openalex_and_openaire
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_and_openaire
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_and_openaire
 as
 (
   select a.doi, row_number() over (order by rand()) as random_order
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_doi_lu_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.openaire_doi_lu_2020_2023 as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_doi_lu_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.openaire_doi_lu_2020_2023 as b on a.doi = b.doi
 );
 
 -- doi_lu_2020_2023_openalex_only
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_only
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_only
 as
 (
   select a.doi, row_number() over (order by rand()) as random_order
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_doi_lu_2020_2023 as a
-  left join cwts-leiden.projectdb_openalex_openaire_2024.openaire_doi_lu_2020_2023 as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_doi_lu_2020_2023 as a
+  left join cwts-leiden.projectdb_openalex_openaire_2025.openaire_doi_lu_2020_2023 as b on a.doi = b.doi
   where b.doi is null
 );
 
 -- doi_lu_2020_2023_openaire_only
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openaire_only
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openaire_only
 as
 (
   select a.doi, row_number() over (order by rand()) as random_order
-  from cwts-leiden.projectdb_openalex_openaire_2024.openaire_doi_lu_2020_2023 as a
-  left join cwts-leiden.projectdb_openalex_openaire_2024.openalex_doi_lu_2020_2023 as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openaire_doi_lu_2020_2023 as a
+  left join cwts-leiden.projectdb_openalex_openaire_2025.openalex_doi_lu_2020_2023 as b on a.doi = b.doi
   where b.doi is null
 );
 
-select count(*) from cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_and_openaire;
---30057
+select count(*) from cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_and_openaire;
+--30374
 
-select count(*) from cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_only;
---4810
+select count(*) from cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_only;
+--4548
 
-select count(*) from cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openaire_only;
---7645
+select count(*) from cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openaire_only;
+--7293
 
 -- doi_lu_2020_2023_openalex_and_openaire_sample
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_and_openaire_sample
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_and_openaire_sample
 as
 (
   select b.random_order, a.*
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_and_openaire as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_and_openaire as b on a.doi = b.doi
   order by random_order
 );
 
 -- doi_lu_2020_2023_openalex_only_sample
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_only_sample
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_only_sample
 as
 (
   select b.random_order, a.*
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openalex_only as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openalex_only as b on a.doi = b.doi
   order by random_order
 );
 
 -- doi_lu_2020_2023_openaire_only_sample
-create or replace table cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openaire_only_sample
+create or replace table cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openaire_only_sample
 as
 (
   select b.random_order, a.*
-  from cwts-leiden.projectdb_openalex_openaire_2024.openalex_openaire_doi_match_2020_2023 as a
-  join cwts-leiden.projectdb_openalex_openaire_2024.doi_lu_2020_2023_openaire_only as b on a.doi = b.doi
+  from cwts-leiden.projectdb_openalex_openaire_2025.openalex_openaire_doi_match_2020_2023 as a
+  join cwts-leiden.projectdb_openalex_openaire_2025.doi_lu_2020_2023_openaire_only as b on a.doi = b.doi
   order by random_order
 );
